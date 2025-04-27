7a77f039f32e8b3b55ebfa1a0d4554d74f8ea2b2
NaviUy <iyu@aventon.com>
2025-04-26 22:18:50 -0700

    PCRE2025

--- pcre2024/src/app/layout.tsx
+++ pcre2024/src/app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from "next";
-import { Inter, Noto_Sans } from "next/font/google";
-import "./globals.css";
+import type {Metadata} from 'next'
+import {Inter, Noto_Sans} from 'next/font/google'
+import './globals.css'
 
 // components
 import Navbar from './components/Navbar.js'
-import { NavbarProvider } from "./components/NavbarContext";
+import {NavbarProvider} from './components/NavbarContext'
 
-const inter = Inter({ subsets: ["latin"], display: 'swap' });
+const inter = Inter({subsets: ['latin'], display: 'swap'})
 const noto_sans = Noto_Sans({
   subsets: ['latin'],
-  display: 'swap'
+  display: 'swap',
 })
 
 export const metadata: Metadata = {
-  title: "PCRE2024",
-  description: "PCRE2024 Event Page",
-};
+  title: 'PCRE',
+  description: 'PCRE Event Page',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
@@ -31,5 +31,5 @@ export default function RootLayout({
         </NavbarProvider>
       </body>
     </html>
-  );
+  )
 }
